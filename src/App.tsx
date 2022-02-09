7ca5ce091eb112ad38c2fa2f5685e8bc6deec292
arnaud-eb <arnaud.depierreux@easebest.com>
2022-02-09 09:43:59 +0100

    Add App parent component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import styled from "styled-components";
+
+import Categories from "./Categories";
+import Menu from "./Menu";
+
+import data from "./data";
+
+const AppWrapper = styled.main`
+  padding: 5rem 0;
+`;
+
+const Title = styled.div`
+  text-align: center;
+  margin-bottom: 2rem;
+
+  h2 {
+    position: relative;
+  }
+
+  h2::after {
+    content: "";
+    position: absolute;
+    top: 130%;
+    right: calc(50% - 5rem / 2);
+    width: 5rem;
+    height: 0.25rem;
+    background: var(--clr-gold);
+  }
+`;
+
+const MenuContainer = styled.section`
+  width: 90vw;
+  margin: 0 auto;
+  max-width: 1170px;
+  display: grid;
+  gap: 3rem 2rem;
+  justify-items: center;
+
+  @media screen and (min-width: 1200px) {
+    width: 95vw;
+    grid-template-columns: 1fr 1fr;
+  }
+`;
 
 function App() {
+  const [menus, setMenus] = React.useState(data);
+
+  const filterMenus = (cat: string) => {
+    if (cat === "all") {
+      setMenus(data);
+    } else {
+      setMenus(data.filter((menu) => menu.category === cat));
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <Title>
+        <h2>our menu</h2>
+      </Title>
+      <Categories data={data} filterMenus={filterMenus} />
+      <MenuContainer>
+        {menus.map((menu) => (
+          <Menu key={menu.id} {...menu} />
+        ))}
+      </MenuContainer>
+    </AppWrapper>
   );
 }
 
